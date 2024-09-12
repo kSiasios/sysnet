ce9bb43338315d52e95063c44826c3994c97f81e
Konstantinos Siasios <kostassiasios1999@gmail.com>
2024-09-12 22:10:01 +0300

    New build and tsconfig

--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getInterfaces = getInterfaces;
-exports.getStatus = getStatus;
+exports.getStatus = exports.getInterfaces = void 0;
 const child_process_1 = require("child_process");
 const os_1 = __importDefault(require("os"));
 function getInterfaces() {
@@ -22,25 +21,20 @@ function getInterfaces() {
         return response;
     });
 }
+exports.getInterfaces = getInterfaces;
 function getStatus() {
     const result = (0, child_process_1.execSync)("netsh interface show interface", {
         encoding: "utf-8",
     });
-    // console.log(result.split(/\r*\n/));
     const lines = result.split(/\r*\n/).filter((line) => line !== "");
-    // console.log(lines);
     const matrix = new Array();
     lines.forEach((line) => {
         const splitLine = line
             .split("  ")
             .filter((elem) => elem !== "")
             .map((cell) => cell.trim());
-        // console.log(splitLine);
         matrix.push(splitLine);
-        // matrix.filter((line) => line.length >= 2);
     });
-    // console.log("MATRIX --->");
-    // matrix.forEach((line) => console.log(line));
     const resultMatrix = matrix.filter((line) => line.length > 1);
     const response = {
         connected: true,
@@ -52,8 +46,6 @@ function getStatus() {
             response.interfaces.push(line[line.length - 1]);
         }
     });
-    // console.log(response);
-    // console.log(matrix.filter((line) => line.length > 1));
-    // console.log(matrix);
     return response;
 }
+exports.getStatus = getStatus;
