c3974f7f08669b1c6f6054d5781c72ed8b3782c1
Konstantinos Siasios <kostassiasios1999@gmail.com>
2024-09-14 15:45:42 +0300

    scanWiFi() implementation

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getStatus = exports.getInterfaces = void 0;
+exports.scanWiFi = exports.getStatus = exports.getInterfaces = void 0;
 const child_process_1 = require("child_process");
 const os_1 = __importDefault(require("os"));
 function getInterfaces() {
@@ -49,3 +49,48 @@ function getStatus() {
     return response;
 }
 exports.getStatus = getStatus;
+function scanWiFi() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const result = (0, child_process_1.execSync)("netsh wlan show networks mode=Bssid", {
+            encoding: "utf-8",
+        });
+        const lines = result
+            .split(/\r*\n/)
+            .filter((line) => line !== "" && line !== " ");
+        const networks = [];
+        let netProps = {
+            ssid: "",
+            type: "",
+            authentication: "",
+            encryption: "",
+            mac: "",
+            signal: 0,
+            radioType: "",
+            band: "",
+            channel: -1,
+            basicRates: [""],
+            otherRates: [""],
+        };
+        for (let index = 0; index < lines.length; index++) {
+            const line = lines[index];
+            const [prop, value] = line.split(" : ");
+            if (prop.includes("SSID")) {
+                netProps.ssid = value;
+                netProps.type = lines[index + 1].split(" : ")[1];
+                netProps.authentication = lines[index + 2].split(" : ")[1];
+                netProps.encryption = lines[index + 3].split(" : ")[1];
+                netProps.mac = lines[index + 4].split(" : ")[1];
+                netProps.signal = parseInt(lines[index + 5].split(" : ")[1].replace("%", ""));
+                netProps.radioType = lines[index + 6].split(" : ")[1];
+                netProps.band = lines[index + 7].split(" : ")[1];
+                netProps.channel = parseInt(lines[index + 8].split(" : ")[1]);
+                netProps.basicRates = lines[index + 9].split(" : ")[1].split(" ");
+                netProps.otherRates = lines[index + 10].split(" : ")[1].split(" ");
+                networks.push(Object.assign({}, netProps));
+                index += 10;
+            }
+        }
+        return networks.sort((a, b) => b.signal - a.signal);
+    });
+}
+exports.scanWiFi = scanWiFi;
